587e6b72acb7113991b6b9f3fc120132aba052f3
rdraghul <rdraghul@icloud.com>
2017-10-03 14:33:30 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,8 @@
+//Hello World should be displayed on the eclipse console.
+
+public class hello {
+     public static void main(String s[])
+     {
+    	 System.out.println("hello world");
+     }
+}
